5bff3b8075015d770e0c1b725682ef143992b1a2
Hareesh <bagayithiharish@gmail.com>
2026-01-09 19:18:24 +0530

    day 5

---
+++ mat1.py
@@ -0,0 +1,23 @@
+m=[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]]
+m1=[]
+mr1=[]
+mr2=[]
+mr3=[]
+print(m)
+for j in range(0,len(m)):
+ for i in range(len(m)):
+        m1.append(m[i][j])
+print(m1)
+print("********************************")
+for i in range (2,-1,-1):
+    mr1.append(m1[i])
+print(mr1)
+for i in range (5,2,-1):
+    mr2.append(m1[i])
+print(mr2)
+for i in range (8,5,-1):
+    mr3.append(m1[i])
+print(mr3)
