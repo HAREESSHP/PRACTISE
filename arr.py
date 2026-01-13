dcde7983ffa973e635f1b6d843d9529f59a986c9
Hareesh <bagayithiharish@gmail.com>
2026-01-13 20:49:48 +0530

    change

--- arr.py
+++ arr.py
@@ -131,7 +131,7 @@ for i in range(n):
     for k in range(i+1):
         print("*", end="")
     for j in range(n-i-1):
-        print(" ", end="")
-    for k in range(i+1):
         print("*", end="")
+    for k in range(i+1):
+        print(" ", end="")
     print() 
