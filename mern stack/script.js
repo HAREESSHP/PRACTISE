30b085da0fc2cb4392bf5ef6b165618ce844d79a
Hareesh <bagayithiharish@gmail.com>
2026-01-27 00:34:41 +0530

    day 11 web

---
+++ mern stack/script.js
@@ -0,0 +1,27 @@
+console.log("Hello, World!");
+const age=58;
+if (age>=18){
+    console.log("You are an adult you can vote.");
+    if(age>+50){
+        console.log("You are eligible for senior citizen benefits.");
+    }
+}
+else{
+    console.log("You are a minor.");
+}
+
+const gradee=90;
+if(gradee>=90){
+    console.log("You got an A+ grade.");
+}else if (gradee>=80){
+    console.log("You got an A grade.");
+}else if (gradee>=70){
+    console.log("You got a B+ grade.");
+}else if (gradee>=60){
+    console.log("You got a B grade.");
+}else{
+    console.log("You need to work harder.");
+}
+console.log("End of the program.");
+const ag="25";
+ag>=18 ? console.log("You are an adult.") : console.log("You are a minor.");
