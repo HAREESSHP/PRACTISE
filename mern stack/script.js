ac1e32ee5f4bf18dbc8448a2cf0b26c44b6dafb5
Hareesh <bagayithiharish@gmail.com>
2026-01-28 22:59:08 +0530

    day 11 web

--- mern stack/script.js
+++ mern stack/script.js
@@ -1,46 +1,69 @@
-console.log("Hello, World!");
-const age=58;
-if (age>=18){
-    console.log("You are an adult you can vote.");
-    if(age>+50){
-        console.log("You are eligible for senior citizen benefits.");
-    }
-}
-else{
-    console.log("You are a minor.");
-}
+// console.log("Hello, World!");
+// const age=58;
+// if (age>=18){
+//     console.log("You are an adult you can vote.");
+//     if(age>+50){
+//         console.log("You are eligible for senior citizen benefits.");
+//     }
+// }
+// else{
+//     console.log("You are a minor.");
+// }
 
-const gradee=90;
-if(gradee>=90){
-    console.log("You got an A+ grade.");
-}else if (gradee>=80){
-    console.log("You got an A grade.");
-}else if (gradee>=70){
-    console.log("You got a B+ grade.");
-}else if (gradee>=60){
-    console.log("You got a B grade.");
-}else{
-    console.log("You need to work harder.");
-}
-console.log("End of the program.");
-const ag="25";
-ag>=18 ? console.log("You are an adult.") : console.log("You are a minor.");
+// const gradee=90;
+// if(gradee>=90){
+//     console.log("You got an A+ grade.");
+// }else if (gradee>=80){
+//     console.log("You got an A grade.");
+// }else if (gradee>=70){
+//     console.log("You got a B+ grade.");
+// }else if (gradee>=60){
+//     console.log("You got a B grade.");
+// }else{
+//     console.log("You need to work harder.");
+// }
+// console.log("End of the program.");
+// const ag="25";
+// ag>=18 ? console.log("You are an adult.") : console.log("You are a minor.");
 
-// looping statemets
-for(let i=0;i<=5;i++){    //for loop
-    console.log("rohan",i);
-}
-count=0;
-while(count<5){
-    console.log("count value is:",count);
-    count++;
-    if(count===3){
-        break;
-    }
-}
-
+// // looping statemets
+// for(let i=0;i<=5;i++){    //for loop
+//     console.log("rohan",i);
+// }
+// count=0;
+// while(count<5){
+//     console.log("count value is:",count);
+//     count++;
+//     if(count===3){
+//         break;
+//     }
+// }
+// named function
 function sum(a,b){
     sum=(a+b);
     return sum;
 }
 console.log(sum(5,10));
+
+// ananymous function
+const greet = function(){
+    console.log("Hello from anonymous function");
+}
+greet();
+
+// arrow function in Es6
+const greet1=()=>{
+    console.log("Hello from arrow function");
+}
+greet1();
+
+// arrow function cocepts
+const squ=(n)=> n*n;
+console.log(squ(5));
+
+const calulate=(a,d,operation)=>{
+    return operation(a,d);
+}
+console.log(calulate(10,5,(x,y)=>x+y)); // addition
+console.log(calulate(10,5,(x,y)=>x-y)); // subtraction
+
