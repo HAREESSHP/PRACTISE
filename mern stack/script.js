a7110fbd2b6ac4f3173f2b42d1f9ef1b0085da13
Hareesh <bagayithiharish@gmail.com>
2026-01-30 18:36:40 +0530

    accesing  the html content

--- mern stack/script.js
+++ mern stack/script.js
@@ -39,78 +39,93 @@
 //     }
 // }
 // named function
-function sum(a,b){
-    sum=(a+b);
-    return sum;
-}
-console.log(sum(5,10));
-
-// ananymous function
-// const greet = function(){
-//     console.log("Hello from anonymous function");
+// function sum(a,b){
+//     sum=(a+b);
+//     return sum;
 // }
-// greet();
+// console.log(sum(5,10));
 
-// // arrow function in Es6
-// const greet1=()=>{
-//     console.log("Hello from arrow function");
-// }
-// greet1();
+// // ananymous function
+// // const greet = function(){
+// //     console.log("Hello from anonymous function");
+// // }
+// // greet();
 
-// arrow function cocepts
-// const squ=(n)=> n*n;
-// console.log(squ(5));
+// // // arrow function in Es6
+// // const greet1=()=>{
+// //     console.log("Hello from arrow function");
+// // }
+// // greet1();
 
-// const calulate=(a,d,operation)=>{
-//     return operation(a,d);
-// }
-// console.log(calulate(10,5,(x,y)=>x+y)); // addition
-// console.log(calulate(10,5,(x,y)=>x-y)); // subtraction
+// // arrow function cocepts
+// // const squ=(n)=> n*n;
+// // console.log(squ(5));
 
-// console.log("one")
-// console.log("two")
-// console.log("three")
-// //content related to synchorinazition
-// setTimeout(function(){
-//     console.log("four")
-// },3000);
+// // const calulate=(a,d,operation)=>{
+// //     return operation(a,d);
+// // }
+// // console.log(calulate(10,5,(x,y)=>x+y)); // addition
+// // console.log(calulate(10,5,(x,y)=>x-y)); // subtraction
 
+// // console.log("one")
+// // console.log("two")
+// // console.log("three")
+// // //content related to synchorinazition
+// // setTimeout(function(){
+// //     console.log("four")
+// // },3000);
+
+// // function getCandies(){
+// //     setTimeout(function(){
+// //         console.log("Here are your candies!")
+// //     },5000);
+// // }
+// // getCandies();
+// // // call back function example
+// // function gree(name,callback){
+// //     console.log("hello",name);
+// //     callback();
+// // }
+// // function saybye(){
+// //     console.log("bye!");
+// // }
+// // gree("hari",saybye);
+// // promise example with synchorinization
+// const ticket=new Promise(function(res,rej){
+//     const seatAvailable=true;
+//     if(seatAvailable){
+//         res("Ticket booked successfully!");
+//     }else{
+//         rej("Ticket booking failed. No seats available.");
+//     }
+// })
+// ticket.then((data)=>{
+//     console.log("on success:",data );
+// }).catch((data)=>{
+//     console.log("on failure:",data);
+// });
+
+// // async await example
 // function getCandies(){
-//     setTimeout(function(){
-//         console.log("Here are your candies!")
-//     },5000);
+//     return new Promise((res,rej) =>{
+//          setTimeout(()=>{
+//             res("Here are your candies!");
+//         },3000);
+//     });
 // }
-// getCandies();
-// // call back function example
-// function gree(name,callback){
-//     console.log("hello",name);
-//     callback();
-// }
-// function saybye(){
-//     console.log("bye!");
-// }
-// gree("hari",saybye);
-// promise example with synchorinization
-const ticket=new Promise(function(res,rej){
-    const seatAvailable=true;
-    if(seatAvailable){
-        res("Ticket booked successfully!");
-    }else{
-        rej("Ticket booking failed. No seats available.");
-    }
-})
-ticket.then((data)=>{
-    console.log("on success:",data );
-}).catch((data)=>{
-    console.log("on failure:",data);
-});
+// console.log(getCandies());
+//accesing the html elements by dom
+console.log(document.body);
+const bodyBackground=document.body;
+bodyBackground.style.backgroundColor="lightblue";
 
-// async await example
-function getCandies(){
-    return new Promise((res,rej) =>{
-         setTimeout(()=>{
-            res("Here are your candies!");
-        },3000);
-    });
-}
-console.log(getCandies());
+// accesing by id name
+const box4=document.getElementById("box-4");
+box4.style.backgroundColor="red";
+box4.style.color="white";
+
+//accesing by tag name
+const h1=document.getElementsByTagName("h1");
+ console.log(h1);
+
+ 
