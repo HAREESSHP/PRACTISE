e85dbf238c52d499268b5d44a4a7f02c583401fe
Hareesh <bagayithiharish@gmail.com>
2026-02-14 22:56:36 +0530

    DSA 167

--- arr.py
+++ arr.py
@@ -135,3 +135,21 @@ for i in range(n):
     for k in range(i+1):
         print(" ", end="")
     print() 
+
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        l=0
+        r=len(numbers)-1
+        while l<r:
+            cs=numbers[l]+numbers[r]
+            if cs == target:
+                return[l+1,r+1]
+            elif cs<target:
+                l=l+1
+            else:
+                r=r-1
