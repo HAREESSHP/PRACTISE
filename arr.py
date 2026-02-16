2c0bf26d050451a710dcc3c5adf569316b2a9fc0
Hareesh <bagayithiharish@gmail.com>
2026-02-16 20:33:12 +0530

    167 change

--- arr.py
+++ arr.py
@@ -145,12 +145,11 @@ class Solution(object):
         """
         l=0
         r=len(numbers)-1
-        cs=0
         while l<r:
-            cs=numbers[l]+numbers[r]
-            if cs == target:
+    
+            if numbers[l]+numbers[r]== target:
                 return[l+1,r+1]
-            elif cs<target:
+            elif numbers[l]+numbers[r]<target:
                 l=l+1
             else:
                 r=r-1
