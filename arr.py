0976eb3d8739ff1f0fe434a9ea0185b15f507f89
Hareesh <bagayithiharish@gmail.com>
2026-02-24 14:43:49 +0530

    longest prefix sum

--- arr.py
+++ arr.py
@@ -265,3 +265,26 @@ def removeDuplicates(self, nums):      #sorted array
                 k += 1
 
         return k
+print("*******************************************************")
+
+def equalSumSpan(self, a1, a2):                   #longest prefix sum
+        n = len(a1)
+        temp = [a1[i] - a2[i] for i in range(n)]
+
+        prefix_sum = 0
+        first_occurrence = {}
+        max_len = 0
+
+        for i in range(n):
+            prefix_sum += temp[i]
+
+            if prefix_sum == 0:
+                max_len = i + 1
+
+            if prefix_sum in first_occurrence:
+                max_len = max(max_len, i - first_occurrence[prefix_sum])
+            else:
+                first_occurrence[prefix_sum] = i
+
+        return max_len
+print("*******************************************************")
