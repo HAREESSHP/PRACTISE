178fa8522f49a65bc7c84f661ebfe44439197c38
Hareesh <bagayithiharish@gmail.com>
2026-03-27 18:14:39 +0530

    change in dsa

--- arr.py
+++ arr.py
@@ -428,11 +428,6 @@ print("*******************************************************")
 
 #binary search
 def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
         l=0
         r=len(nums)-1
         while(l<=r):
