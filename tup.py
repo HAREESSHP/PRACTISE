52bf80500813cb6e7ffbab79b465483a07e7cb89
Hareesh <bagayithiharish@gmail.com>
2026-05-30 00:25:43 +0530

    binary tree

--- tup.py
+++ tup.py
@@ -134,3 +134,29 @@ while temp>0:
     sum+=digit**3
     temp//=10
 print(f"{n} is an amstrong number") if n==sum else print(f"{n} is not an amstrong number")
+
+#binary tree
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+class BinaryTree:
+    def __init__(self):
+        self.root=None
+    def insert(self,data):
+        if self.root is None:
+            self.root=Node(data)
+        else:
+            self._insert(data,self.root)
+    def _insert(self,data,node):
+        if data<node.data:
+            if node.left is None:
+                node.left=Node(data)
+            else:
+                self._insert(data,node.left)
+        else:
+            if node.right is None:
+                node.right=Node(data)
+            else:
+                self._insert(data,node.right)
