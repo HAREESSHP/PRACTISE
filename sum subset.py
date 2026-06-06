52993dba20deeff9489a48723802135c73ebf1cd
Hareesh <bagayithiharish@gmail.com>
2026-06-06 21:16:12 +0530

    colour map

--- sum subset.py
+++ sum subset.py
@@ -18,3 +18,24 @@ class Solution(object):
             possible |= new_possible
         
         return target in possible
+
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        left=0
+        mid = 0
+        right=len(nums)-1
+        while(mid<=right):
+            if nums[mid]==0:
+                nums[left],nums[mid]=nums[mid],nums[left]
+                left=left+1
+                mid=mid+1
+            elif nums[mid]==1:
+                mid=mid+1
+            else:
+                nums[mid], nums[right] = nums[right], nums[mid]
+                right -= 1
+        return nums
