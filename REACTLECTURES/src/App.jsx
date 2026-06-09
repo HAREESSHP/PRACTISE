1bea65f75f40a33869f2d437de8a251c05ff55d6
Hareesh <bagayithiharish@gmail.com>
2026-06-09 21:05:53 +0530

    started with nav bar

--- REACTLECTURES/src/App.jsx
+++ REACTLECTURES/src/App.jsx
@@ -13,6 +13,15 @@ function App() {
 
   return (
     <>
+    <nav className='nav'>
+      <img id="logo" src="/public/profile.jpg.png" alt="logo" className='logo' />
+      <div className="nav-links">
+        <button id="home" >HOME</button>
+        <button id="about">ABOUT</button>
+        <button id="services">SERVICES</button>
+        <button id="contact">CONTACT</button>
+      </div>
+    </nav>
     <h1>NORMAL PRACTICE</h1>
     <p>These is an incrementer</p>
     <p>value : <span  style={{ color: 'orange' }}>{count}</span></p>
