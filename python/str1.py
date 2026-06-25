3304436040161434f2290d97a3894c35f02ad541
Hareesh <bagayithiharish@gmail.com>
2026-06-25 11:51:49 +0530

    count prefixes of a string

--- python/str1.py
+++ python/str1.py
@@ -154,3 +154,17 @@ class Solution(object):
             else:
                 break
         return nums[i]
+
+
+    def countPrefixes(self, words, s):
+        """
+        :type words: List[str]
+        :type s: str
+        :rtype: int
+        """
+        count=0
+        for word in words:
+            if s.startswith(word):
+                count += 1
+        return count
+        
