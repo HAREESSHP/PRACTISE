dd4fa378be21655f5c790c92fc11a0b67b56723d
Hareesh <bagayithiharish@gmail.com>
2026-06-26 18:36:42 +0530

    Third max element

--- python/str1.py
+++ python/str1.py
@@ -180,3 +180,15 @@ class Solution(object):
             if ch =="1":
                 count=count+1
         return count
+    
+#Third max element
+    def thirdMax(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums=list(set(nums))
+        nums.sort(reverse=True)
+        if len(nums)>=3:
+            return nums[2]
+        return nums[0]
