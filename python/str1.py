476369c0eae06f881d7541fbc77225db889cabc1
Hareesh <bagayithiharish@gmail.com>
2026-06-29 08:35:54 +0530

    #search in rotated sorted array

--- python/str1.py
+++ python/str1.py
@@ -246,3 +246,28 @@ class Solution(object):
                     r=i
             else:i=0
         return r
+
+#search in rotated sorted array
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l=0
+        r=len(nums)-1
+        while (l<=r):
+            mid=(l+r)//2
+            if nums[mid]==target:
+                return mid
+            if nums[l] <= nums[mid]:
+                if nums[l] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return -1
