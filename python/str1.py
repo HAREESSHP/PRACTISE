5696d376c4a5bba9f89548fc6c87f1c8366d14e2
Hareesh <bagayithiharish@gmail.com>
2026-07-01 15:27:33 +0530

    #bad version

--- python/str1.py
+++ python/str1.py
@@ -292,3 +292,27 @@ class Solution(object):
         else:
             n=len(password)
             return 6-n
+
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+#bad version
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left = 1
+        right = n
+
+        while left < right:
+            mid = left + (right - left) // 2
+
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
