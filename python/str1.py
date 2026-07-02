36557de909b9ab8a5b437a9e8d12aa5998b722e3
Hareesh <bagayithiharish@gmail.com>
2026-07-02 10:39:24 +0530

    #Valid paranthasis

--- python/str1.py
+++ python/str1.py
@@ -316,3 +316,27 @@ class Solution(object):
                 left = mid + 1
 
         return left
+
+#Valid paranthasis
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack=[]
+        pairs = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        }
+
+        for ch in s:
+            if ch in "([{":
+                stack.append(ch)
+            else:
+                if not stack:
+                    return False
+                top = stack.pop()
+                if top != pairs[ch]:
+                    return False
+        return len(stack) == 0
