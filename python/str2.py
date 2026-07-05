aca8054f91649e195b5d687f52fc9e0f1c1e0f93
Hareesh <bagayithiharish@gmail.com>
2026-07-05 11:12:53 +0530

    #unigue element of a string

--- python/str2.py
+++ python/str2.py
@@ -23,3 +23,22 @@ print("*************************************************")
 
 name=input("enter your name ")
 print(name)
+
+
+#unigue element of a string
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        freq={}
+        for ch in s:
+            freq[ch]=freq.get(ch,0)+1
+        for ch in t:
+            if ch not in freq or freq[ch]==0:
+                return ch
+            freq[ch]-=1
+
+
+   
