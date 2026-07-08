07f13ca8b294c30c83eb641aa4fcf94daba55e68
Hareesh <bagayithiharish@gmail.com>
2026-07-08 21:42:18 +0530

    #height checker

--- python/str2.py
+++ python/str2.py
@@ -166,3 +166,16 @@ print(name)
 
         backtrack(0)
         return res
+
+#height checker
+    def heightChecker(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        expected=sorted(heights)
+        count=0
+        for i in range(len(heights)):
+            if heights[i] != expected[i]:
+                count=count+1
+        return count
