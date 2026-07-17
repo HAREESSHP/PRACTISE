2e08223ddfa7caa88c20c53ae0a2d2ccd050b2e3
Hareesh <bagayithiharish@gmail.com>
2026-07-17 23:59:22 +0530

    #left and ryt diff

--- python/factorial.py
+++ python/factorial.py
@@ -5,3 +5,23 @@ def factorial(n):
     else:
         return n*factorial(n-1)
 print("Factorial of", n, "is", factorial(n))
+
+
+#left and ryt diff
+    def leftRightDifference(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        le=[0]*len(nums)
+        re=[0]*len(nums)
+        for i in range(1,len(nums)):
+            le[i]=nums[i-1]+le[i-1]
+        for i in range(len(nums)-2,-1,-1):
+            re[i]=nums[i+1]+re[i+1]
+        ans=[]
+        for i in range(len(nums)):
+            ans.append(abs(le[i]-re[i]))
+        return ans
+
+        
