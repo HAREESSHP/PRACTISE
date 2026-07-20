17614290f4237f6e78ba9f10d83e711f5304b203
Hareesh <bagayithiharish@gmail.com>
2026-07-20 21:17:42 +0530

    #Maximum Product Subarray

--- python/factorial.py
+++ python/factorial.py
@@ -92,3 +92,24 @@ def leftRightDifference(self, nums):
             su=ans[i]+gain[i]
             ans.append(su)
         return max(ans)
+
+#Maximum Product Subarray
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ans = nums[0]
+        max_p = nums[0]
+        min_p = nums[0]
+
+        for i in range(1, len(nums)):
+            if nums[i] < 0:
+                max_p, min_p = min_p, max_p
+
+            max_p = max(nums[i], max_p * nums[i])
+            min_p = min(nums[i], min_p * nums[i])
+
+            ans = max(ans, max_p)
+
+        return ans
