ad465a2fa0ed96adaf7ce9c6d1218fcfa069bcb5
Hareesh <bagayithiharish@gmail.com>
2026-07-24 23:50:01 +0530

    changes

--- python/factorial.py
+++ python/factorial.py
@@ -213,3 +213,26 @@ def leftRightDifference(self, nums):
             if z != 0 and num % z == 0:
                 count += 1
         return count
+#Power n
+    def myPow(self, x: float, n: int) -> float:
+
+        def calc_power(x, n):
+            if x == 0:
+                return 0
+            if n == 0:
+                return 1
+            
+            res = calc_power(x, n // 2)
+            res = res * res
+
+            if n % 2 == 1:
+                return res * x
+            
+            return res
+
+        ans = calc_power(x, abs(n))
+
+        if n >= 0:
+            return ans
+        
+        return 1 / ans 
