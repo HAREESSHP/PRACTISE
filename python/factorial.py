15b823600de19862a74ffe2f1be4a29c0dc922ff
Hareesh <bagayithiharish@gmail.com>
2026-07-25 21:38:21 +0530

    #count primes

--- python/factorial.py
+++ python/factorial.py
@@ -276,4 +276,25 @@ def leftRightDifference(self, nums):
                 ans = max(ans, li[i] * li[j])
         return ans
 
-        
+#count primes
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 2:
+            return 0
+
+        prime = [True] * n
+        prime[0] = False
+        prime[1] = False
+
+        p = 2
+        while p * p < n:
+            if prime[p]:
+                for i in range(p * p, n, p):
+                    prime[i] = False
+            p += 1
+
+        return sum(prime)
+   
