efe00060ed108e9925b7ee5f28c0f125e524cdd0
Hareesh <bagayithiharish@gmail.com>
2026-08-03 21:43:48 +0530

    #Daily temperatures

--- python/factorial.py
+++ python/factorial.py
@@ -491,3 +491,19 @@ def leftRightDifference(self, nums):
             ans[i]*=rp
             rp*=nums[i]
         return ans
+
+#Daily temperatures
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
+        stack=[]
+        ans=[0]*len(temperatures)
+        for i in range (len(temperatures)):
+            while stack and temperatures[i]>temperatures[stack[-1]]:
+                index=stack.pop()
+                ans[index]=i-index
+            stack.append(i)
+        return ans
+        
