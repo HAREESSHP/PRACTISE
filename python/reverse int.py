7ef42be8deb272ddc55d7d84ff307b89077e3f5e
Hareesh <bagayithiharish@gmail.com>
2026-08-09 22:33:40 +0530

    #mainimum diff

--- python/reverse int.py
+++ python/reverse int.py
@@ -39,3 +39,16 @@ print(solution.reverse(-123))  # Output: -321
             if sum(customer)>m:
                 m=sum(customer)
         return m
+#minimum diff
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        if len(nums)==1:
+            return 0
+        left=0
+        ans=float(inf)
+        for r in range(len(nums)):
+            if r-left==k:
+                left=left+1
+            if r-left+1==k:
+                ans=min(ans,nums[r]-nums[left])
+        return ans
