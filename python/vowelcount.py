dec3bc8cd6a73a42535d39c8fb1c6ee01ae221ba
Hareesh <bagayithiharish@gmail.com>
2026-08-13 23:22:53 +0530

    middle of a list

--- python/vowelcount.py
+++ python/vowelcount.py
@@ -8,3 +8,23 @@ for i in s:
         vowel_count+=1
 print(vowel_count)
 print(li)
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        s=[]
+        current=head
+        while current is not None:
+            s.append(current)
+            current=current.next
+        n=len(s)//2
+        return s[n]
+        
