0f3e2a69ab46132b1728e15945186885e128a618
Hareesh <bagayithiharish@gmail.com>
2026-08-18 22:35:24 +0530

    largest element

--- python/vowelcount.py
+++ python/vowelcount.py
@@ -28,3 +28,32 @@ class Solution(object):
         n=len(s)//2
         return s[n]
         
+
+class Solution(object):
+    def largestInteger(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        n = len(nums)
+        if k == 1:
+            count = Counter(nums)
+            ans = -1
+
+            for x in nums:
+                if count[x] == 1:
+                    ans = max(ans, x)
+
+            return ans
+        if k == n:
+            return max(nums)
+        count = Counter(nums)
+        ans = -1
+        if count[nums[0]] == 1:
+            ans = max(ans, nums[0])
+
+        if count[nums[-1]] == 1:
+            ans = max(ans, nums[-1])
+
+        return ans
