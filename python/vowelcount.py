7e402f61f1558b735119f01312ebd388e9bb0b45
Hareesh <bagayithiharish@gmail.com>
2026-08-20 22:58:15 +0530

    #attendence 1

--- python/vowelcount.py
+++ python/vowelcount.py
@@ -56,3 +56,25 @@ class Solution(object):
             ans = max(ans, nums[-1])
 
         return ans
+
+#attendence 1
+    def checkRecord(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        absent = 0
+        late = 0
+
+        for ch in s:
+            if ch == 'A':
+                absent += 1
+                late = 0
+            elif ch == 'L':
+                late += 1
+            else:
+                late = 0
+
+            if absent >= 2 or late >= 3:
+                return False
+        return True
