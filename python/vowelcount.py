a357a648bd56d07d3a7439a9eaf7eeffe356c2ff
Hareesh <bagayithiharish@gmail.com>
2026-08-28 22:58:14 +0530

    leetcode

--- python/vowelcount.py
+++ python/vowelcount.py
@@ -110,16 +110,12 @@ class Solution(object):
 from collections import deque
 
 class RecentCounter:
-
     def __init__(self):
         self.queue=deque()
-
     def ping(self, t: int) -> int:
         self.queue.append(t)
-
         while self.queue[0]<t-3000:
             self.queue.popleft()
-
         return len(self.queue) 
 
 
