f9bd1e86b06252d3c6c43b1305545aed20714329
Hareesh <bagayithiharish@gmail.com>
2026-08-30 16:01:25 +0530

    #roman to number

--- python/vowelcount.py
+++ python/vowelcount.py
@@ -115,7 +115,21 @@ class RecentCounter:
             self.queue.popleft()
         return len(self.queue) 
 
-
-# Your RecentCounter object will be instantiated and called as such:
-# obj = RecentCounter()
-# param_1 = obj.ping(t)
+#roman to number
+    def romanToInt(self, s: str) -> int:
+        roman = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        ans = 0
+        for i in range(len(s)):
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i+1]]:
+                ans = ans - roman[s[i]]
+            else:
+                ans = ans + roman[s[i]]
+        return ans
