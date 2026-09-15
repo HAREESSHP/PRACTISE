65f3e22f5f51f44f1ffb0ae80580d37893640dfb
Hareesh <bagayithiharish@gmail.com>
2026-09-15 09:28:26 +0530

    fizz buzz

--- python/vowelcount.py
+++ python/vowelcount.py
@@ -155,3 +155,19 @@ class RecentCounter:
             ans = max(ans, nums[-1])
 
         return ans
+
+class Solution:
+    def fizzBuzz(self, n: int) -> list[str]:
+        ans = []
+
+        for i in range(1, n + 1):
+            if i % 15 == 0:
+                ans.append("FizzBuzz")
+            elif i % 3 == 0:
+                ans.append("Fizz")
+            elif i % 5 == 0:
+                ans.append("Buzz")
+            else:
+                ans.append(str(i))
+
+        return ans
